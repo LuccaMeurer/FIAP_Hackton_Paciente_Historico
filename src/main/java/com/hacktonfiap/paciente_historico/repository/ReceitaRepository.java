52e165fd046e399be233dcdc717b07a9e31d6892
LuccaMeurer <luccameurer1@gmail.com>
2026-02-12 17:30:52 -0300

    Add ReceitaRepository

---
+++ src/main/java/com/hacktonfiap/paciente_historico/repository/ReceitaRepository.java
@@ -0,0 +1,12 @@
+package com.hacktonfiap.paciente_historico.repository;
+
+import com.hacktonfiap.paciente_historico.entity.Receita;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReceitaRepository extends JpaRepository<Receita, Long> {
+
+    List<Receita> findByPacienteId(Long pacienteId);
+
+}
