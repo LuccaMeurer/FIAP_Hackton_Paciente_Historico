e0f34a6831afa34eb5056d3a94fcb9abfe29e969
LuccaMeurer <luccameurer1@gmail.com>
2026-02-12 17:31:47 -0300

    Add Usuario Service

---
+++ src/main/java/com/hacktonfiap/paciente_historico/service/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.hacktonfiap.paciente_historico.service;
+
+import com.hacktonfiap.paciente_historico.entity.Usuario;
+import com.hacktonfiap.paciente_historico.repository.UsuarioRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UsuarioService(UsuarioRepository usuarioRepository,
+                          PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Usuario salvar(Usuario usuario) {
+
+        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario buscarPorEmail(String email) {
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+}
