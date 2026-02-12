6c2cfcfbe60e8eb5ebd2923b2d86363026e82b1d
LuccaMeurer <luccameurer1@gmail.com>
2026-02-12 17:28:26 -0300

    Add UsuarioRepository

--- src/main/java/com/hacktonfiap/paciente_historico/repository/UsuarioRepository.java
+++ src/main/java/com/hacktonfiap/paciente_historico/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package com.hacktonfiap.paciente_historico.repository;
 
-public interface UsuarioRepository {
+import com.hacktonfiap.paciente_historico.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
 }
